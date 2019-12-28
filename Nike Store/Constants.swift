6ce6fb6376c4e0f4eaf055cf51371a4575d2bb8a
Mustafa Alsoffi <alsoffimustafa@gmail.com>
2019-12-28 21:18:33 +0800

    Added more colors and shapes like the price tag and its color

--- Nike Store/Constants.swift
+++ Nike Store/Constants.swift
@@ -13,6 +13,8 @@ struct K {
     static var cellNibName = "ShoeCellCollectionViewCell"
     struct UIColors {
         static var deepCove = UIColor(red:0.07, green:0.06, blue:0.25, alpha:1.0)
+        static var darkBlue = UIColor(red:0.01, green:0.05, blue:0.13, alpha:1.0)
+        static var flatPink = UIColor(red:0.91, green:0.11, blue:0.30, alpha:1.0)
     }
     
     struct CellIDs {
--- Nike Store/Views/ShoeCellCollectionViewCell.swift
+++ Nike Store/Views/ShoeCellCollectionViewCell.swift
@@ -11,9 +11,16 @@ import UIKit
 class ShoeCellCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var shoeImageView: UIImageView!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    @IBOutlet weak var priceTagView: UIView!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        priceLabel.backgroundColor = K.UIColors.darkBlue
+        priceTagView.backgroundColor = K.UIColors.flatPink
     }
 
 }
